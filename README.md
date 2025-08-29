a854a0dffa8a90a240340b7ac7f1662e642bc68a
ni <andynitt@163.com>
2025-08-29 09:32:17 +0800

    merge with gitee

---
+++ README.md
@@ -0,0 +1,2 @@
+dev branch to develop
+if rules meets will merge to master branch
---
+++ bootmanager/common.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "stdio.h"
+#include "stdlib.h"
+
+/*result of bootcheckmanager*/
+typedef enum
+{
+	Boot_Check_SUCCESS,
+	Boot_Check_FAIL
+}StdReturnType;
+
+#endif
